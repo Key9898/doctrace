08653b759ba4647e9094776a1f14c7ebccdd6907
Wunna Aung <wunna301292@gmail.com>
2026-06-17 13:36:59 +0700

    chore: remove samples, scratch, sources, log files, and generators from main branch tracking

--- scratch/print_text.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-const data = JSON.parse(
-  fs.readFileSync("./samples/sample-bank-statements.json", "utf8"),
-);
-console.log("Text field for document 0 page 0:");
-console.log(JSON.stringify(data.documents[0].pages[0].text));
